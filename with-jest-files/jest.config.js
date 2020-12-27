26b12387b260ab652b7e9c4bbc853533e4cc8369
Arseniy Ivanov <freeatnet@freeatnet.com>
2020-12-27 18:44:28 +0100

    Make source files more lint-rules compliant

--- with-jest-files/jest.config.js
+++ with-jest-files/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   setupFilesAfterEnv: ['./jest.setup.js'],
   moduleNameMapper: {
-    "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js"
+    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
   },
 };
