445541241b2e66509f590abcf6c69549d0b64f4f
Arseniy Ivanov <freeatnet@freeatnet.com>
2020-12-27 18:40:19 +0100

    Introduce `yarn lint:precommit`
    A combination of `fix` and `ci`'s `--max-warnings=0` for a cleaner codebase

--- with-eslint-files/lint-staged.config.js
+++ with-eslint-files/lint-staged.config.js
@@ -1,6 +1,6 @@
 // For configuration options, see
 // https://github.com/okonet/lint-staged#configuration
 module.exports = {
-  '*.{js,ts,tsx}': 'yarn lint:ci',
+  '*.{js,ts,tsx}': 'yarn lint:precommit',
   '*.ts?(x)': () => 'yarn check-types',
 };
